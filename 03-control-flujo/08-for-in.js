3d8bb2e967ddcb7430d78f00a957d0ba960a58a2
Ventura-02 <aabrahamuno@gmail.com>
2024-11-23 11:09:15 -0600

    adding 2.0

---
+++ 03-control-flujo/08-for-in.js
@@ -0,0 +1,14 @@
+let user = {
+  id: 1,
+  name: "Chanchito Feliz",
+  age: 25,
+};
+
+for (let prop in user) {
+  console.log(prop, user[prop]);
+}
+
+let animales = ["Chanchito Feliz", "Dragón", "Perrito"];
+for (let animal in animales) {
+  console.log(indice, animales[indice]);
+}
---
+++ 03-control-flujo/10-switch.js
@@ -0,0 +1,14 @@
+let accion = "actualizar";
+
+switch (accion) {
+  case "listar":
+    console.log("Acción de listar");
+    break;
+
+  case "guardar":
+    console.log("Acción de guardar");
+    break;
+
+  default:
+    console.log("Accion no reconocida");
+}
